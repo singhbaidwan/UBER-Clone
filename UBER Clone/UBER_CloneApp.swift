617db6d75712cd055c55efde3d180726bef363d7
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2022-10-02 00:36:53 +0530

    Initial Commit

---
+++ UBER Clone/UBER_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UBER_CloneApp.swift
+//  UBER Clone
+//
+//  Created by Dalveer singh on 02/10/22.
+//
+
+import SwiftUI
+
+@main
+struct UBER_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
